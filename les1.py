57ff6989a06462421c8e816b273dc5d34f047f48
RomanBodnar <hr7jd9wem4ew@gmail.com>
2025-09-05 16:43:48 +0300

    code40

--- les1.py
+++ les1.py
@@ -6,15 +6,57 @@
 
 # -----------------завдання 2-----------
 
-# word = input("Введіть рядок: ")
-# if word:
-#     word = word[0].lower() + word[1:]
-# if word == word[::-1]:
-#     print(f"{word} - це паліндром")
+# text = input("Введіть рядок: ")
+# if text == text[::-1]:
+#     print(f"{text} - це паліндром")
 # else:
-#     print(f"{word} - не паліндром")
+#     print(f"{text} - не паліндром")
 
 # -----------------завдання 3-----------
 
-# word = input("Введіть рядок: ")
+# text = input("Введіть текст: ")
+# reserved = "python itstep teacher student roman taras"
+# reservedlist = text.split()
+# for i in range(len(reservedlist)):
+#     if reservedlist[i].lower() in reserved:
+#         reservedlist[i] = reservedlist[i].upper()
+# result = " ".join(reservedlist)
+# print(f"{result}")
 
+# -----------------завдання 4-----------
+
+# text = input("Введіть рядок: ")
+# sumvol1 = input("Введіть перший символ: ")
+# sumvol2 = input("Введіть другий символ: ")
+# search1 = text.find(sumvol1)
+# search2 = text.find(sumvol2)
+# if search1 != -1 and search2 != -1 and search1 < search2:
+#     result = text[:search1] + text[search2+1:]
+# else:
+#     result = text
+# print(f"Результат: {result}")
+
+# -----------------завдання 5-----------
+
+# text = input("Введіть текст: ")
+# symbols = input("Введіть символи: ")
+# symbols_set = set(symbols)
+# words = text.split()
+# result_words = []
+# for i in words:
+#     f = False
+#     for c in i:
+#         if c in symbols_set:
+#             f = True
+#             break
+#     if not f:
+#         result_words.append(i)
+# result = " ".join(result_words)
+# print(f"Результат: {result}")
+
+# -----------------завдання 6-----------
+
+# text = input("Введіть текст: ")
+# text1 = text.split()
+# result = " ".join(text1[::-1])
+# print(result)
